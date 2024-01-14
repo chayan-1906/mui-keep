a526436209b9456e524b9105181b8420001675b5
Padmanabha Das <padmanabhadas9647@gmail.com>
2024-01-14 23:22:19 +0530

    attempted to create PWA

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import Home from './components/Home.jsx'
 import DataProvider from './context/DataProviderContext.jsx'
+import {useEffect} from 'react'
 
 function App() {
+    useEffect(() => {
+        let deferredPrompt
+
+        window.addEventListener('beforeinstallprompt', (event) => {
+            event.preventDefault()
+            deferredPrompt = event
+        })
+
+        // Add logic to show install button and handle user clicks
+    }, [])
+    
     return (
         <DataProvider>
             <Home/>
