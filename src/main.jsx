38d1a13cefec77caa08e6eed3b174350c1eb86aa
Padmanabha Das <padmanabhadas9647@gmail.com>
2024-01-16 20:50:03 +0530

    localhost doesn't show perper icon

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,6 @@ import App from './App.jsx'
 import {ThemeProvider} from '@mui/material'
 import theme from './globals/Theme.jsx'
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js', { scope: '/' })
-        .then(registration => {
-            console.log('Service Worker registered with scope:', registration.scope);
-        })
-        .catch(error => {
-            console.error('Service Worker registration failed:', error);
-        });
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <ThemeProvider theme={theme}>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'MUI Keep',
+        short_name: 'MUI Keep',
+        theme_color: '#000',
+      },
+      strategies: ['cacheFirst', 'networkFirst'],
+    }),
+  ],
 })
